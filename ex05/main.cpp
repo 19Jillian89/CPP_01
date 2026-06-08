ebbd17160c1900bca4016d9f05323e904034c850
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-06-08 15:36:54 +0200

    Create main.cpp

---
+++ ex05/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilnassi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/08 15:36:31 by ilnassi           #+#    #+#             */
+/*   Updated: 2026/06/08 15:36:40 by ilnassi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("UNKNOWN");
+
+    return 0;
+}
