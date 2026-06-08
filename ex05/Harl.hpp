29c9730ae4cff651a96f87039422c74e1a026b7d
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-06-08 16:08:20 +0200

    Refactor Harl class member function declarations

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -17,14 +17,14 @@
 
 class Harl
 {
-        private:
-                void debug(void);
-                void info(void);
-                void warning(void);
-                void error(void);
-
-        public:
-                void complain(std::string level);
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	
+	public:
+		void complain(const std::string& level);
 };
 
 #endif
